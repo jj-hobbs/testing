75f247fa4d0b6ec725628edaa06e0066889ff235
justin.hobbs <justinhobbs@fastmail.com>
2022-12-12 22:43:46 -0600

    Initial Commit

---
+++ testing/testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingApp.swift
+//  testing
+//
+//  Created by Justin Hobbs on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
